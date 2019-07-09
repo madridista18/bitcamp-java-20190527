b85082471f76c69a2eb9f5aa4553ada34bedd5b7
madridista18 <madridista18@naver.com>
2019-07-09 17:25:26 +0900

    bitcamp-java-application 13 version

--- bitcamp-java-application/src/main/java/com/eomcs/lms/handler/BoardHandler.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/handler/BoardHandler.java
@@ -6,11 +6,11 @@ import com.eomcs.lms.domain.Board;
 import com.eomcs.lms.util.Input;
 
 public class BoardHandler {
-  private static Board[] boards = new Board[100];
-  private static int boardsSize = 0;
+  private Board[] boards = new Board[100];
+  private int boardsSize = 0;
   public static Scanner keyScan;
   
-  public static void addBoard() {
+  public void addBoard() {
     Board board = new Board();
     board.no = Input.getIntValue("번호? ");
     board.contents = Input.getStringValue("내용? ");
@@ -20,7 +20,7 @@ public class BoardHandler {
     System.out.println("저장하였습니다. ");
   }
 
-  public static void listBoard() {
+  public void listBoard() {
     for (int i = 0; i < boardsSize; i++) {
       Board board = boards[i];
       System.out.printf("%s, %s, %s\n", 
