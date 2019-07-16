fde19ef47a00c2795ebfdd045327dd1da27bd3ab
madridista18 <madridista18@naver.com>
2019-07-16 16:06:12 +0900

    bitcamp-java-application 19 version

--- bitcamp-java-application/src/main/java/com/eomcs/util/ArrayList.java
+++ bitcamp-java-application/src/main/java/com/eomcs/util/ArrayList.java
@@ -48,5 +48,74 @@ public class ArrayList<E> {
   public int size() {
     return this.size;
   }
+  
+  @SuppressWarnings("unchecked")
+  public E get(int index) {
+    if (index < 0 || index >= size) // 유효 하지않은 인덱스라면 예외발생!
+      throw new IndexOutOfBoundsException(String.format("인덱스 = %d", index));
+    
+    return (E) list[index];
+  }
+  
+  @SuppressWarnings("unchecked")
+  public E set(int index, E obj) {
+    if (index < 0 || index >= size)
+      throw new IndexOutOfBoundsException(String.format("인덱스 = %d", index));
+    
+    E old = (E) list[index];
+    list[index] = obj;
+    
+    return old;
+  }
+  
+  public E remove(int index) {
+    if (index < 0 || index >= size)
+      throw new IndexOutOfBoundsException(String.format("인덱스 = %d", index));
+    
+    @SuppressWarnings("unchecked")
+    E old = (E) list[index];
+
+    // 방법1: 직접 반복문을 이용하여 삭제 처리하기
+//    for (int i = index + 1; i < size; i++) {
+//      list[i-1] = list[i];
+//    }
+//    list[--size] = null;
+    
+    // 방법2: 배열 복사 기능을 이용하여 삭제 처리하기 
+    System.arraycopy(list, index + 1, list, index, size - (index + 1));
+    
+    // 값을 삭제한 후 맨 끝 값이 들어 있던 방을 null로 설정한다.
+    // => 레퍼런스가 남아있지 않게 하여 가비지가 정상적으로 이뤄지도록 한다. 
+    list[--size] = null;
+    
+    return old; 
+  }
+  
+  public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("000");
+    list.add("111");
+    list.add("222");
+    list.add("333");
+    list.add("444");
+    list.add("555");
+    
+    String old = list.remove(5);
+    System.out.println("원래 값: " + old);
+    System.out.println("------------------------");
+    
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i));
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
 
