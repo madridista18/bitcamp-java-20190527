028a9718e30ce2e04a21c46e6f2ae7a4928ec0ad
madridista18 <madridista18@naver.com>
2019-07-25 21:21:35 +0900

    bitcamp-java-application v30

---
+++ bitcamp-java-application/v30/src/main/java/com/eomcs/lms/handler/MemberDetailCommand.java
@@ -0,0 +1,43 @@
+package com.eomcs.lms.handler;
+
+import java.util.List;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.util.Input;
+
+public class MemberDetailCommand implements Command {
+  private List<Member> list;
+  private Input input;
+
+  public MemberDetailCommand(Input input, List<Member> list) {
+    this.input = input;
+    this.list = list;
+  }
+
+  @Override
+  public void execute() {
+    int no = input.getIntValue("번호? ");
+
+    Member member = null;
+    for (int i = 0; i < list.size(); i++) {
+      Member temp = list.get(i);
+      if (temp.getNo() == no) {
+        member = temp;
+        break;
+      }
+    }
+    if (member == null) {
+      System.out.println("해당 번호의 데이터가 없습니다. ");
+      return;
+    }
+    
+    System.out.println("이름: " + member.getName());
+    System.out.println("이메일: " + member.getEmail());
+    System.out.println("암호: " + member.getPassword());
+    System.out.println("사진: " + member.getPhoto());
+    System.out.println("전화: " + member.getPhoneNumber());
+    System.out.println("가입일: " + member.getRegisteredDate());
+  }
+}
+
+
+
