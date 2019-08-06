3cc92bf3de809726b7d7065dc7017e64626e0f52
madridista18 <madridista18@naver.com>
2019-08-06 14:22:50 +0900

    client v33_2

--- bitcamp-java-application-server/src/main/java/com/eomcs/lms/ServerApp.java
+++ bitcamp-java-application-server/src/main/java/com/eomcs/lms/ServerApp.java
@@ -1,4 +1,4 @@
-// v33_2: Stateful 통신 방식을 Stateless 통신 방식으로 변경하여 더 많은 클라이언트 요청 처리.  
+// v33_3: 클라이언트 요청마다 서블릿 객체를 만들지 않기 
 package com.eomcs.lms;
 
 import java.io.ObjectInputStream;
@@ -7,18 +7,17 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 import com.eomcs.lms.context.ServletContextListener;
-import com.eomcs.lms.dao.BoardDao;
-import com.eomcs.lms.dao.LessonDao;
-import com.eomcs.lms.dao.MemberDao;
-import com.eomcs.lms.servlet.BoardServlet;
-import com.eomcs.lms.servlet.LessonServlet;
-import com.eomcs.lms.servlet.MemberServlet;
+import com.eomcs.lms.servlet.Servlet;
 
 public class ServerApp {
 
   ArrayList<ServletContextListener> listeners = new ArrayList<>();
   int port;
+
+  //서버가 실행되는 동안 공유할 객체를 보관하는 저장소를 준비한다. 
+  HashMap<String,Object> servletContext = new HashMap<>();
 
   public ServerApp(int port) {
     this.port = port;
@@ -30,9 +29,6 @@ public class ServerApp {
 
     try (ServerSocket serverSocket = new ServerSocket(this.port)) {
       System.out.println("서버 시작!");
-
-      //서버가 실행되는 동안 공유할 객체를 보관하는 저장소를 준비한다. 
-      HashMap<String,Object> servletContext = new HashMap<>();
 
       // 서버가 시작되면 보고를 받을 관찰자(Observer)에게 보고한다. 
       for (ServletContextListener listener : listeners) {
@@ -46,31 +42,19 @@ public class ServerApp {
 
           System.out.println("클라이언트와 연결되었음.");
 
-          BoardDao boardDao = (BoardDao) servletContext.get("boardDao");
-          MemberDao memberDao = (MemberDao) servletContext.get("memberDao");
-          LessonDao lessonDao = (LessonDao) servletContext.get("lessonDao");
-
-          BoardServlet boardServlet = new BoardServlet(boardDao, in, out);
-          MemberServlet memberServlet = new MemberServlet(memberDao, in, out);
-          LessonServlet lessonServlet = new LessonServlet(lessonDao, in, out);
-
           // 클라이언트가 보낸 명령을 읽는다.
           String command = in.readUTF();
           System.out.println(command + " 요청 처리중...");
 
-          if (command.startsWith("/board/")) {
-            boardServlet.service(command);
+          Servlet servlet = null;
 
-          } else if (command.startsWith("/member/")) {
-            memberServlet.service(command);
-
-          } else if (command.startsWith("/lesson/")) {
-            lessonServlet.service(command);
-
-          } else if (command.equals("serverstop")) {
+          if (command.equals("serverstop")) {
             break;
 
-          } else {
+          } else if ((servlet = findServlet(command)) != null) {
+            servlet.service(command, in, out);
+
+          } else { 
             out.writeUTF("fail");
             out.writeUTF("지원하지 않는 명령입니다.");
           }
@@ -98,7 +82,20 @@ public class ServerApp {
   // => 즉, 서블릿을 실행하는데 필요한 환경을 준비시키는 객체를 등록한다. 
   public void addServletContextListener(ServletContextListener listener) {
     listeners.add(listener);
+  }
 
+  private Servlet findServlet(String command) {
+    Set<String> keys = servletContext.keySet();
+
+    // 명령어의 포함 된 키를 찾아서 해당 키로 저장된 서블릿을 꺼낸다. 
+    // => 명령(/board/list) : 키(/board/)
+    for (String key : keys) {
+      if (command.startsWith(key)) {
+        return (Servlet) servletContext.get(key);
+      }
+    }
+    // => 명령(/files/list) : 키(?)
+    return null;
   }
 
   public static void main(String[] args) {
