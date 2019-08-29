b88b60a2d9ee47160568a1513fa9dee3e92301ee
madridista18 <madridista18@naver.com>
2019-08-29 11:15:51 +0900

    v44_1

---
+++ bitcamp-java-application4-server/v44_1/src/main/java/com/eomcs/lms/handler/PhotoBoardUpdateCommand.java
@@ -0,0 +1,117 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.util.List;
+import com.eomcs.lms.dao.PhotoBoardDao;
+import com.eomcs.lms.dao.PhotoFileDao;
+import com.eomcs.lms.domain.PhotoBoard;
+import com.eomcs.lms.domain.PhotoFile;
+import com.eomcs.util.Input;
+import com.eomcs.util.PlatformTransactionManager;
+
+public class PhotoBoardUpdateCommand implements Command {
+
+  private PlatformTransactionManager txManager; 
+  private PhotoBoardDao photoBoardDao;
+  private PhotoFileDao photoFileDao;
+
+  // BoardHandler가 사용하는 Input 객체를 반드시 설정하도록 강제해보자!
+  // => Input 객체처럼 어떤 작업을 실행하기 위해 반드시 있어야 하는 객체를 
+  //   "의존 객체(dependency)"라 부른다. 
+  // => 의존 객체를 강제로 설정하게 만드는 방법? 
+  //   다음과 같이 의존 객체를 넘겨 받는 생성자를 정의하는 것이다.
+  public PhotoBoardUpdateCommand(
+      PlatformTransactionManager txManager, PhotoBoardDao photoBoardDao, PhotoFileDao photoFileDao) {
+    this.txManager = txManager;
+    this.photoBoardDao = photoBoardDao;
+    this.photoFileDao = photoFileDao;
+  }
+
+  @Override
+  public void execute(BufferedReader in, PrintStream out) {
+    try {
+      txManager.beginTransaction();
+      int no = Input.getIntValue(in, out, "번호? ");
+
+      PhotoBoard photoBoard = photoBoardDao.findBy(no);
+      if (photoBoard == null) {
+        out.println("해당 번호의 데이터가 없습니다. ");
+        return;
+      }
+
+      out.println("제목을 입력하지 않으면 이전 제목을 유지합니다. ");
+      String str = Input.getStringValue(in, out, 
+          String.format("제목(%s)? ", photoBoard.getTitle()));
+
+      // 제목을 입력했으면 사진 게시글의 제목을 변경한다.
+      if (str.length() > 0) {
+        photoBoard.setTitle(str);
+        photoBoardDao.update(photoBoard);
+        out.println("게시물의 제목을 변경하였습니다.");
+      }
+
+      // 이전에 등록한 파일 목록을 출력한다. 
+      out.println("사진 파일:");
+      List<PhotoFile> files = photoFileDao.findAll(no);
+      for (PhotoFile file : files) {
+        out.printf("> %s\n", file.getFilePath());
+      }
+
+      // 파일을 변경할 지 여부를 묻는다. 
+      out.println("사진은 일부만 변경할 수 없습니다.");
+      out.println("전체를 새로 등록해야 합니다. ");
+      String response = Input.getStringValue(in, out,
+          "사진을 변경하시겠습니까?(Y/N)");
+
+      if (!response.equalsIgnoreCase("y")) {
+        out.println("파일 변경을 취소합니다.");
+        return;
+      }
+
+      // 기존 사진 파일을 삭제한다.
+      photoFileDao.deleteAll(no);
+
+      out.println("최소 한 개의 사진 파일을 등록해야 합니다.");
+      out.println("파일명 입력 없이 그냥 엔터를 치면 파일 추가를 마칩니다.");
+      out.flush();
+
+      int count = 0;
+      while (true) {
+        String filepath = Input.getStringValue(in, out, "사진파일? ");
+        if (filepath.length() == 0) {
+          if (count > 0 ) {
+            break;
+          } else {
+            out.println("최소 한 개의 사진 파일을 등록해야 합니다. ");
+            continue;
+          }
+        }
+        PhotoFile photoFile = new PhotoFile();
+        photoFile.setFilePath(filepath);
+        photoFile.setBoardNo(photoBoard.getNo());
+        photoFileDao.insert(photoFile);
+        count++;
+      }
+      txManager.commit();
+      out.println("사진을 변경하였습니다. ");
+
+    } catch (Exception e) {
+      try {txManager.rollback();} catch (Exception e2) {}
+      // 예외가 발생하면 DBMS의 임시데이터베이스에 보관된 데이터 변경 작업들을 모두 취소한다. 
+      out.println("해당 사진을 찾을 수 없습니다.");
+      System.out.println(e.getMessage());
+
+    } 
+  }
+}
+
+
+
+
+
+
+
+
+
+
