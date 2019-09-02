1acc19398922a3178f492f1c5350a671aeef241f
madridista18 <madridista18@naver.com>
2019-09-02 12:51:48 +0900

    v49_1

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/LoginCommand.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/LoginCommand.java
@@ -9,22 +9,16 @@ import com.eomcs.util.Component;
 import com.eomcs.util.Input;
 import com.eomcs.util.RequestMapping;
 
-@Component("/auth/login")
+@Component
 public class LoginCommand {
-
   private MemberDao memberDao;
 
-  // BoardHandler가 사용하는 Input 객체를 반드시 설정하도록 강제해보자!
-  // => Input 객체처럼 어떤 작업을 실행하기 위해 반드시 있어야 하는 객체를 
-  //    "의존 객체(dependency)"라 부른다. 
-  // => 의존 객체를 강제로 설정하게 만드는 방법? 
-  //    다음과 같이 의존 객체를 넘겨 받는 생성자를 정의하는 것이다.
   public LoginCommand(MemberDao memberDao) {
     this.memberDao = memberDao;
   }
   
-  @RequestMapping // 클라이언트 요청이 들어 왔을 때 이 메서드를 호출하라고 표시한다. 
-  public void execute(BufferedReader in, PrintStream out) {
+  @RequestMapping("/member/login") // 클라이언트 요청이 들어 왔을 때 이 메서드를 호출하라고 표시한다. 
+  public void login(BufferedReader in, PrintStream out) {
     try {
       HashMap<String,Object> params = new HashMap<>();
       params.put("email", Input.getStringValue(in, out, "이메일?"));
@@ -43,5 +37,8 @@ public class LoginCommand {
       System.out.println(e.getMessage());
     }
   }
+
 }
+
+
 
