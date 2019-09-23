8156d8a9d97b65bf281334001cceb01824047596
madridista18 <madridista18@naver.com>
2019-09-23 21:51:46 +0900

    v57_3

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
@@ -1,8 +1,11 @@
-// v57_3 : JSP에 JSTL 적용하기 
+// v58_2 : Page Controller를 일반 클래스(POJO:Plain Old Java Object)로 전환하기  
 package com.eomcs.lms;
 
 // 작업:
-// => /webapp/jsp/**/*.jsp 변경
+// => Command 인터페이스 생성
+// => XxxServlet을  XxxCommand 로 전환
+// => 커맨드 객체의 관리 주체를 서블릿 컨테이너에서 Spring IoC 컨테이너로 이전.
+// => DispatcherServlet 변경 
 //
 //
 // dummy 클래스!
