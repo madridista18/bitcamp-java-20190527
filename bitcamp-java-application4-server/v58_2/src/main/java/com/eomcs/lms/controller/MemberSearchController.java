7854f9a496d50febd50e04bf4d782b4640e58168
madridista18 <madridista18@naver.com>
2019-09-24 10:24:58 +0900

    v58_3

---
+++ bitcamp-java-application4-server/v58_2/src/main/java/com/eomcs/lms/controller/MemberSearchController.java
@@ -0,0 +1,30 @@
+package com.eomcs.lms.controller;
+
+import java.util.List;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import com.eomcs.lms.controller.PageController;
+import com.eomcs.lms.dao.MemberDao;
+import com.eomcs.lms.domain.Member;
+
+@Component("/member/search")
+public class MemberSearchController implements PageController {
+
+  @Resource
+  private MemberDao memberDao;
+
+  @Override
+  public String execute(HttpServletRequest request, HttpServletResponse response) 
+      throws Exception {
+
+    response.setContentType("text/html;charset=UTF-8");
+    List<Member> members = memberDao.findByKeyword(
+        request.getParameter("keyword"));
+
+    request.setAttribute("members", members);
+    return "/jsp/member/search.jsp";
+
+  }
+}
---
+++ bitcamp-java-application4-server/v58_2/src/main/java/com/eomcs/lms/controller/PhotoBoardDetailController.java
@@ -0,0 +1,33 @@
+package com.eomcs.lms.controller;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import com.eomcs.lms.dao.PhotoBoardDao;
+import com.eomcs.lms.domain.PhotoBoard;
+
+@Component("/photoboard/detail")
+public class PhotoBoardDetailController implements PageController {
+
+  @Resource
+  private PhotoBoardDao photoBoardDao;
+
+  @Override
+  public String execute(HttpServletRequest request, HttpServletResponse response) 
+      throws Exception {
+
+    response.setContentType("text/html;charset=UTF-8");
+    int no = Integer.parseInt(request.getParameter("no"));
+
+    PhotoBoard photoBoard = photoBoardDao.findWithFilesBy(no);
+    if (photoBoard == null) {
+      throw new Exception("해당 번호의 데이터가 없습니다!");
+    }
+    photoBoardDao.increaseViewCount(no);
+
+    request.setAttribute("photoBoard", photoBoard);
+    return "/jsp/photoboard/detail.jsp";
+
+  }
+}
