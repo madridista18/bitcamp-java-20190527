218ab9cb089f5f2e3b9b7a7a89e281d4a43b3448
madridista18 <madridista18@naver.com>
2019-09-26 18:14:52 +0900

    bitcamp-java-spring-webmvc

--- bitcamp-java-spring-webmvc/src/main/java/bitcamp/config/App1WebApplicationInitializer.java
+++ bitcamp-java-spring-webmvc/src/main/java/bitcamp/config/App1WebApplicationInitializer.java
@@ -1,10 +1,20 @@
 package bitcamp.config;
 
+import java.io.File;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class App1WebApplicationInitializer 
   extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+  String uploadTmpDir;
+  
+  public App1WebApplicationInitializer() {
+    uploadTmpDir = new File(System.getProperty("java.io.tmpdir")).getAbsolutePath();
+    System.out.println("업로드 임시폴더: " + uploadTmpDir);
+  }
+  
   @Override
   protected Class<?>[] getRootConfigClasses() {
     return null;
@@ -24,6 +34,27 @@ public class App1WebApplicationInitializer
   protected String getServletName() {
     return "app1";
   }
+  
+  
+    
+  @Override
+  protected void customizeRegistration(Dynamic registration) {
+    // Servlet 3.0 API의 파일 업로드를 사용하려면 다음과 같이 
+    // DispatcherServlet 에 설정을 추가해야한다. 
+    // 만약 Spring의 방식으로 파일 업로드를 처리하고 싶다면, 
+    // AppConfig.java의 MultipartResolve
+    
+    // DispatcherServlet이 multipart/form-data으로 전송된 데이터를 처리하려면
+    // 해당 콤포넌트를 등록해야한다. 
+    
+//    registration.setMultipartConfig(new MultipartConfigElement(
+//        uploadTmpDir, // 업로드 한 파일을 임시 보관할 위치
+//        10000000, // 최대 업로드할 수 있는 파일들의 총 크기 
+//        15000000, // 요청 전체 데이터의 크기
+//        2000000 // 업로드 되고 있는 파일을 메모리에 임시 보관하는 크기 
+//        ));
+  }
+  
 
 }
 
